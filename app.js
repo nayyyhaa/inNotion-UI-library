75b351423f29cca73d3b8f9ff3470e32c39e15b9
nayyyhaa <nehaguptang.ng@gmail.com>
2022-01-28 01:43:27 +0530

    add: badge + button components

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*----------TRANSITION ALERT------------*/
 const transitionAlert = document.querySelector(".transition-alert");
 const demoClose = document.querySelector(".demo-close");
 const reopenBtn = document.querySelector(".reopen-alert");
@@ -13,3 +14,25 @@ reopenBtn.addEventListener("click", () => {
   transitionAlert.classList.remove("removeTransition");
   transitionAlert.style.display = "flex";
 });
+
+/*-----------TOGGLE + HEART BADGE---------------------*/
+
+// toggle badge
+const toggleBadgeBtn = document.querySelector(".toogle-badge-btn");
+const toggleBadge = document.querySelector(".toogle-badge-btn .badge");
+
+toggleBadgeBtn.addEventListener("click", () => {
+  toggleBadge.classList.toggle("hide");
+});
+
+// toggle icon badge
+const toggleIconBadgeBtn = document.querySelector(".toogle-icon-badge-btn");
+const iconBadgeBtn = document.querySelector(".toogle-icon-badge-btn i");
+const toggleIconBadge = document.querySelector(".toogle-icon-badge-btn .badge");
+
+toggleIconBadgeBtn.addEventListener("click", () => {
+  let isLiked = iconBadgeBtn.classList.contains("fa-heart-o");
+  iconBadgeBtn.className = isLiked ? "fa fa-heart" : "fa fa-heart-o";
+  toggleIconBadge.textContent = isLiked ? +toggleIconBadge.innerText + 1 : +toggleIconBadge.innerText - 1;
+});
+
